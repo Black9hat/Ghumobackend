556406c75dd23dfd70aeaaca2a6f46ee9b029eda
star0tex <you@example.com>
2026-03-03 13:37:14 +0530

    env header fix

--- src/server.js
+++ src/server.js
@@ -88,7 +88,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
+allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning', 'x-admin-token'],
   exposedHeaders: ['Content-Range', 'X-Content-Range']
 }));
 
